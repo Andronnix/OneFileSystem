59fa6d6ff6caf616a28cb228babf0307eb763c1c
Andronnix <a.d.kokorev@yandex.ru>
2020-06-19 15:20:15 +0300

    Added tests for TempFileController

--- FileSystem/src/test/java/ofs/TempFileControllerTest.java
+++ FileSystem/src/test/java/ofs/TempFileControllerTest.java
@@ -1,36 +1,200 @@
 package ofs;
 
+import ofs.controller.TempFileController;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.nio.channels.Channels;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashSet;
+import java.util.Set;
+
 public class TempFileControllerTest {
+    private TempFileController controller;
 
-
-    @Test
-    public void nonExistingFilesDontExist() {
-
+    @Before
+    public void createController() throws IOException {
+        controller = new TempFileController();
     }
 
-    /*
-    SeekableByteChannel newByteChannel(OFSPath path, Set<? extends OpenOption> options, FileAttribute<?>... attrs) throws IOException;
+    @Test
+    public void nonExistingFilesDontExist() throws IOException {
+        Assert.assertFalse(controller.exists(Path.of("does_not_exist")));
+        Assert.assertFalse(controller.exists(Path.of("does_not_exist", "also_does_not_exist")));
+    }
 
-    DirectoryStream<Path> newDirectoryStream(OFSPath dir, DirectoryStream.Filter<? super Path> filter) throws IOException;
+    @Test(expected = NoSuchFileException.class)
+    public void deletingNonExistingFileCausesException() throws IOException {
+        controller.delete(Path.of("does_not_exist"));
+    }
 
-    boolean exists(OFSPath path) throws IOException;
+    @Test
+    public void createsNewFile() throws IOException {
+        var path = Path.of("plain_file");
+        controller.newByteChannel(path, Set.of(StandardOpenOption.CREATE));
+        Assert.assertTrue(controller.exists(path));
 
-    void createDirectory(OFSPath dir, FileAttribute<?>... attrs) throws IOException;
+        var attrs = controller.readAttributes(path, BasicFileAttributes.class);
+        Assert.assertFalse(attrs.isDirectory());
+    }
 
-    void delete(OFSPath path) throws IOException;
+    @Test
+    public void deletesExistingFile() throws IOException {
+        var path = Path.of("plain_file");
+        controller.newByteChannel(path, Set.of(StandardOpenOption.CREATE));
+        Assert.assertTrue(controller.exists(path));
 
-    void copy(OFSPath source, OFSPath target, CopyOption... options) throws IOException;
+        controller.delete(path);
+        Assert.assertFalse(controller.exists(path));
+    }
 
-    void move(OFSPath source, OFSPath target, CopyOption... options) throws IOException;
+    @Test
+    public void createsNewDirectory() throws IOException {
+        var path = Path.of("dir");
+        controller.createDirectory(path);
+        Assert.assertTrue(controller.exists(path));
 
-    <V extends FileAttributeView> V getFileAttributeView(OFSPath path, Class<V> type, LinkOption... options);
+        var attrs = controller.readAttributes(path, BasicFileAttributes.class);
+        Assert.assertTrue(attrs.isDirectory());
+    }
 
-    <A extends BasicFileAttributes> A readAttributes(OFSPath path, Class<A> type, LinkOption... options) throws IOException;
+    @Test(expected = DirectoryNotEmptyException.class)
+    public void doesntDeleteNonEmptyDir() throws IOException {
+        var dir = Path.of("some_dir");
+        var file = Path.of("some_dir", "some_file");
+        var file2 = Path.of("some_dir", "some_file");
+        controller.createDirectory(dir);
+        controller.newByteChannel(file, Set.of(StandardOpenOption.CREATE));
+        controller.newByteChannel(file2, Set.of(StandardOpenOption.CREATE));
 
-    Map<String, Object> readAttributes(OFSPath path, String attributes, LinkOption... options) throws IOException;
+        controller.delete(dir);
+    }
 
-    void setAttribute(OFSPath path, String attribute, Object value, LinkOption... options) throws IOException;
-    */
+    @Test
+    public void createsMultipleFiles() throws IOException {
+        var file = Path.of("some_file");
+        var file2 = Path.of("another_file");
+
+        controller.newByteChannel(file, Set.of(StandardOpenOption.CREATE));
+        controller.newByteChannel(file2, Set.of(StandardOpenOption.CREATE));
+
+        Assert.assertTrue(controller.exists(file));
+        Assert.assertTrue(controller.exists(file2));
+    }
+
+    @Test
+    public void createsDirInsideDir() throws IOException {
+        var dir = Path.of("dir");
+        var inner = Path.of("dir", "inner_dir");
+
+        controller.createDirectory(dir);
+        controller.createDirectory(inner);
+
+        Assert.assertTrue(controller.exists(dir));
+        Assert.assertTrue(controller.exists(inner));
+    }
+
+    @Test
+    public void createsFileInsideDir() throws IOException {
+        var dir = Path.of("dir");
+        var inner = Path.of("dir", "inner_file");
+
+        controller.createDirectory(dir);
+        controller.newByteChannel(inner, Set.of(StandardOpenOption.CREATE));
+
+        Assert.assertTrue(controller.exists(dir));
+        Assert.assertTrue(controller.exists(inner));
+    }
+
+    @Test
+    public void copiesFiles() throws IOException {
+        var dir = Path.of("dir");
+        controller.createDirectory(dir);
+
+        var src = Path.of("dir", "src");
+        var target = Path.of("dir", "target");
+
+        controller.newByteChannel(src, Set.of(StandardOpenOption.CREATE));
+
+        controller.copy(src, target);
+
+        Assert.assertTrue(controller.exists(src));
+        Assert.assertTrue(controller.exists(target));
+    }
+
+    @Test
+    public void movesFiles() throws IOException {
+        var dir = Path.of("dir");
+        controller.createDirectory(dir);
+
+        var src = Path.of("dir", "src");
+        var target = Path.of("dir", "target");
+
+        controller.newByteChannel(src, Set.of(StandardOpenOption.CREATE));
+
+        controller.move(src, target);
+
+        Assert.assertFalse(controller.exists(src));
+        Assert.assertTrue(controller.exists(target));
+    }
+
+    @Test
+    public void copiesFileContents() throws IOException {
+        var magicNumber = 12;
+        var dir = Path.of("dir");
+        controller.createDirectory(dir);
+
+        var src = Path.of("dir", "src");
+        var target = Path.of("dir", "target");
+
+        var bc = controller.newByteChannel(src, Set.of(StandardOpenOption.CREATE));
+        var outputStream = Channels.newOutputStream(bc);
+        outputStream.write(magicNumber);
+        outputStream.close();
+
+        controller.copy(src, target);
+
+        var copyBc = controller.newByteChannel(target, Set.of(StandardOpenOption.CREATE));
+        var inputStream = Channels.newInputStream(copyBc);
+
+        Assert.assertEquals(magicNumber, inputStream.read());
+        Assert.assertEquals(0, inputStream.available());
+
+        inputStream.close();
+    }
+
+    @Test
+    public void movesDirectoriesToAnotherLevel() throws IOException {
+        var dir = Path.of("dir");
+        controller.createDirectory(dir);
+
+        var src = Path.of("src");
+        controller.createDirectory(src);
+
+        var target = Path.of("dir", "target");
+
+        controller.move(src, target);
+
+        Assert.assertFalse(controller.exists(src));
+        Assert.assertTrue(controller.exists(target));
+    }
+
+    @Test
+    public void providesCorrectDirectoryStream() throws IOException {
+        Set<Integer> created = new HashSet<>();
+        for(int i = 0; i < 100; i++) {
+            controller.createDirectory(Path.of("src", Integer.toString(i)));
+            created.add(i);
+        }
+
+        controller.newDirectoryStream(Path.of("src"), p -> true)
+                .forEach(p -> Assert.assertTrue( // Ensure we don't have extra dirs we didn't create
+                        created.remove(Integer.parseInt(p.getName(0).toString()))
+                ));
+
+        Assert.assertTrue(created.isEmpty()); // Ensure we seen all dirs we have created
+    }
 }
