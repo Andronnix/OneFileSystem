d76ba3d62953bd4f0843cd8848c8594dc487c1fe
Andronnix <a.d.kokorev@yandex.ru>
2020-06-20 23:53:38 +0300

    Added simple app showing jb logo in an example.

--- Example/src/main/java/Main.java
+++ Example/src/main/java/Main.java
@@ -1,14 +1,43 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        var fs = FileSystems.newFileSystem(URI.create("ofs:/"), Map.of());
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        var m = new Main();
+        m.copyImage();
+        m.showImage();
+    }
 
-        System.out.println(fs.isOpen());
+    void copyImage() throws IOException, URISyntaxException {
+        FileSystems.newFileSystem(URI.create("ofs:]=$"), Map.of());
+
+        var resourcePath = Path.of(Main.class.getResource("/jetbrains.png").toURI());
+        var targetPath = Path.of(URI.create("ofs:]=$logo.png"));
+
+        Files.copy(resourcePath, targetPath);
+    }
+
+    void showImage() throws IOException {
+        var is = Files.newInputStream(Path.of(URI.create("ofs:]=$logo.png")));
+
+        BufferedImage img = ImageIO.read(is);
+        ImageIcon icon = new ImageIcon(img);
+        JFrame frame = new JFrame();
+        frame.setLayout(new FlowLayout());
+        frame.setSize(600,600);
+        JLabel lbl = new JLabel();
+        lbl.setIcon(icon);
+        frame.add(lbl);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
