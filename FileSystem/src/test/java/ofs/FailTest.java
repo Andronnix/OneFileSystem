9a45cbf2cfdc741c107cc0822efb01c58aa39495
andrey.kokorev <andrey.kokorev@jetbrains.com>
2020-08-18 12:14:25 +0300

    nofail

--- FileSystem/src/test/java/ofs/FailTest.java
+++ FileSystem/src/test/java/ofs/FailTest.java
@@ -5,8 +5,8 @@ import org.junit.Test;
 
 public class FailTest {
     @Test
-    public void fail1() throws InterruptedException {
+    public void nofail() throws InterruptedException {
         Thread.sleep(5000);
-        Assert.fail();
+        //Assert.fail();
     }
 }
